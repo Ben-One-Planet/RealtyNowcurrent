ad56b5e48bfd688f418fe84d4702c47dae98183b
Ben Beatty <benbeatty@MOB-BenB.local>
2021-03-02 16:21:12 -0500

    Added functionality to housing options with javascript

--- functions.js
+++ functions.js
@@ -45,3 +45,15 @@ $(document).ready(function () {
     })
 
 });
+
+
+// Add active class to the current button (highlight it)
+var header = document.getElementById("buttonDIV");
+var btns = header.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function () {
+        var current = document.getElementsByClassName("active");
+        current[0].className = current[0].className.replace(" active", "");
+        this.className += " active";
+    });
+}
